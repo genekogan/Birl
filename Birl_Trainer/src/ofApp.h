445792668a4ff01e78163cfe9f76405041eef7a0
Gene Kogan <kogan.gene@gmail.com>
2014-05-17 02:33:31 -0400

    added osc receiver for birl input data

---
+++ Birl_Trainer/src/ofApp.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#define PORT 1234
+
+#include "ofMain.h"
+#include "ofxLearn.h"
+#include "ofxUI.h"
+#include "ofxOsc.h"
+
+class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void trainFromFile(string path);
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+	void guiEvent(ofxUIEventArgs &e);
+    
+private:
+    ofxUISuperCanvas *guiData, *guiOptions;
+    ofxLearn mlp;
+    ofxOscReceiver osc;
+    vector<float> keys;
+    float label;
+    bool recording;
+    bool testing;
+};
---
+++ README.md
@@ -0,0 +1,10 @@
+machine learning framework for the Birl, using ofxLearn and ofxUI.
+
+## Birl_Trainer
+
+This application receives Birl input data and trains a neural net (multilayer perceptron) mapping to synthesis parameters.
+
+todo:
+interface for performer to choose synthesis parameters during recording
+implement grid parameter search to optimize regression accuracy
+saving/loading models, presets
